d8e09c35225897a67bcb19049d6aa947090eb475
maxud <mlenon13@gmail.com>
2017-08-21 12:45:18 +0300

    добавил проект

---
+++ lesson_4_hw/lesson_4_hwTests/lesson_4_hwTests.m
@@ -0,0 +1,39 @@
+//
+//  lesson_4_hwTests.m
+//  lesson_4_hwTests
+//
+//  Created by maxud on 21.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lesson_4_hwTests : XCTestCase
+
+@end
+
+@implementation lesson_4_hwTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
